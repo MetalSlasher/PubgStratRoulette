f28ad4310e6c45f9d1b9e4b15c226c2abaf2f7d9
David Salzmann <david.salzmann24@gmail.com>
2017-06-06 21:54:32 +0200

    Added own mobile background
    Added own mobile background

--- script.js
+++ script.js
@@ -226,5 +226,10 @@ function MobilePage()
 {
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) 
-    document.getElementById('main').innerHTML = "<img src='underconstruction.jpg'>";
+{
+        
+document.body.style.backgroundImage = "url('backgroundmobile.jpg')";
+    
+}
+    
 }
